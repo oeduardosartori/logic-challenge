24c4ca5ecfe9cfb35b65251408caa7951736cff0
Eduardo Sartori <edusartori00@gmail.com>
2024-08-06 14:36:23 -0300

    feat: create exercises

---
+++ easy/exercise-01/solution.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    # Divisão inteira
+    int_division = a // b
+    
+    # Divisão de ponto flutuante
+    float_division = a / b
+    
+    # Imprimir os resultados
+    print(int_division)
+    print(float_division)
