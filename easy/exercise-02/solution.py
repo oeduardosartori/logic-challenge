c682f19a6cdf2d9bf6dacc3831e812cc19180a1f
EasyCo <eduardobyeasyco@gmail.com>
2024-08-07 09:20:24 -0300

    feat: add new task and solution (#1)

---
+++ easy/exercise-02/solution.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print(i**2)
+        
+        
+""" 
+Explicação:
+    n = int(input()): Lê um inteiro n da entrada padrão.
+    for i in range(n):: Itera sobre todos os inteiros não negativos menores que n.
+    print(i**2): Calcula e imprime o quadrado de i.
+"""
